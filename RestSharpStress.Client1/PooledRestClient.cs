cc74a8ee5b349716f790e4b783593f0f90fa0d64
Milosz Krajewski <milosz.krajewski@gmail.com>
2024-03-04 17:56:30 +0000

    wip

---
+++ RestSharpStress.Client1/PooledRestClient.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using Microsoft.Extensions.ObjectPool;
+using RestSharp;
+
+namespace RestSharpStress.Client1;
+
+/// <summary>
+/// PooledRestClient is just a RestClient that uses an ObjectPool to manage the HttpMessageHandler
+/// instances. It exposes <see cref="Acquire"/> factory method instead of a constructor, to make it
+/// clear that the instances might not be really new. It has internal static pool and keeps track
+/// of handlers so they can be returned to the pool when PooledRestClient is disposed.
+/// </summary>
+public class PooledRestClient: RestClient
+{
+	private static readonly HttpMessageHandlerPool Pool = new(1024);
+
+	private HttpMessageHandler? _handler;
+
+	public static RestClient Acquire(ConfigureRestClient configure) =>
+		new PooledRestClient(configure, Pool.Get());
+
+	private PooledRestClient(ConfigureRestClient configure, HttpMessageHandler handler):
+		base(handler, false, configure)
+	{
+		_handler = handler;
+	}
+
+	protected override void Dispose(bool disposing)
+	{
+		// prevent double-disposal
+		var handler = Interlocked.Exchange(ref _handler, null);
+
+		// if GC decides to dispose the object (disposing is false), we cannot be sure
+		// if handler is still reusable, so we are not returning it
+		if (disposing && handler is not null)
+		{
+			Pool.Return(handler);
+		}
+
+		base.Dispose(disposing);
+	}
+}
+
+/// <summary>
+/// This class might not exist at all, but extracted it so we can easily have many pools,
+/// configured separately (and have slightly less typing in the main class).
+/// </summary>
+/// <param name="maximumRetained">Maximum number of handlers to keep in the pool.</param>
+internal class HttpMessageHandlerPool(int maximumRetained):
+	DefaultObjectPool<HttpMessageHandler>(new HttpMessageHandlerPoolPolicy(), maximumRetained);
+
+internal class HttpMessageHandlerPoolPolicy:
+	IPooledObjectPolicy<HttpMessageHandler>
+{
+	private static long _created;
+
+	// maximum age we keep the handler (with caveats, but roughly true)
+	private static readonly TimeSpan MaximumAge = TimeSpan.FromMinutes(15);
+
+	/// <summary>Tracking number of handlers created, for debugging purposes only.</summary>
+	public static long Created => Interlocked.Read(ref _created);
+
+	public HttpMessageHandler Create()
+	{
+		Interlocked.Increment(ref _created);
+		return new TimestampedHttpMessageHandler(new HttpClientHandler());
+	}
+
+	public bool Return(HttpMessageHandler handler) =>
+		handler is not TimestampedHttpMessageHandler timestamped ||
+		timestamped.Age <= MaximumAge;
+}
+
+public class TimestampedHttpMessageHandler(HttpMessageHandler handler):
+	DelegatingHandler(handler)
+{
+	private readonly DateTime _timestamp = DateTime.UtcNow;
+
+	public TimeSpan Age => DateTime.UtcNow - _timestamp;
+}
---
+++ RestSharpStress.Client1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace RestSharpStress.Client1;
+
+internal class Program
+{
+	private const int Threads = 128;
+
+	private static readonly TaskCompletionSource<int> Start = new();
+	private static readonly CancellationTokenSource Stop = new();
+	private static long _finished;
+	private static long _failed;
+
+	public static void Main(string[] args)
+	{
+		var loops = Enumerable.Range(0, Threads).Select(_ => StartLoop()).ToArray();
+		var monitor = Task.Run(Monitor);
+
+		Start.SetResult(0);
+		Console.CancelKeyPress += (_, e) => {
+			Console.WriteLine("Stopping...");
+			e.Cancel = true;
+			Stop.Cancel();
+		};
+
+		Task.WaitAll(loops);
+		monitor.Wait();
+
+		Console.WriteLine("Stopped");
+	}
+
+	private static async Task Monitor()
+	{
+		await Start.Task;
+		while (!Stop.IsCancellationRequested)
+		{
+			await Task.Delay(1000);
+			Console.WriteLine(
+				$"Finished: {_finished}, Failed: {_failed}, Clients: {HttpMessageHandlerPoolPolicy.Created}");
+		}
+	}
+
+	private static async Task StartLoop()
+	{
+		while (!Stop.IsCancellationRequested)
+		{
+			try
+			{
+				_ = await GetTime();
+				Interlocked.Increment(ref _finished);
+			}
+			catch (Exception)
+			{
+				Interlocked.Increment(ref _failed);
+			}
+		}
+	}
+
+	private static async Task<DateTime> GetTime()
+	{
+		// NOTE: Dispose is crucial, it is the mechanism that returns the handler to the pool
+		using var client = GetClient();
+		return await client.GetJsonAsync<DateTime>("/");
+	}
+
+	private static RestClient GetClient() =>
+		UseOursPooledClient();
+
+	//----------------------------------------------------------------------------------------------
+
+	private static RestClient UseNewClient() =>
+		new(c => c.BaseUrl = new Uri("http://localhost:5222"));
+
+	//----------------------------------------------------------------------------------------------
+
+	private static RestClient? _shared;
+
+	private static RestClient UseSharedClient() =>
+		_shared ??= UseNewClient();
+
+	//----------------------------------------------------------------------------------------------
+
+	private static RestClient UseTheirPooledClient() =>
+		new(c => c.BaseUrl = new Uri("http://localhost:5222"), null, null, true);
+
+	//----------------------------------------------------------------------------------------------
+
+	private static RestClient UseOursPooledClient() =>
+		PooledRestClient.Acquire(c => c.BaseUrl = new Uri("http://localhost:5222"));
+}
